e06d691d0e9ce4690eec7ed9f01b9df29ed244b3
Suyog Mishal <suyog15122@gmail.com>
2019-03-12 11:33:17 +0530

    scape6

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,15 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use FOS\RestBundle\Controller\Annotations as Rest;
 
+
+
+/**
+ * Class LoginController
+ * @package App\Controller
+ */
 class LoginController extends AbstractController
 {
     /**
-     * @Route("/login",name="security_login")
+     * @Rest\Post("/api/login", name="security_login")
      */
     public function login(){
         return $this->redirectToRoute('homepage');
--- src/Controller/PropertiesController.php
+++ src/Controller/PropertiesController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\ScapeProperties;
 use App\Repository\FeaturedRepository;
 use App\Repository\ScapePropertiesRepository;
 use Doctrine\ORM\EntityManagerInterface;
